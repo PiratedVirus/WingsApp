69d385a51c0b8770f0e7c340ab3de4812e03b56c
Saurabh Kulkarni <saurabhk201@gmail.com>
2017-09-12 11:47:38 +0530

    ALPHA 10.3
    - Added Elegance completely

--- app/src/main/java/com/saurabh/wings2017/Elegance.java
+++ app/src/main/java/com/saurabh/wings2017/Elegance.java
@@ -159,62 +159,183 @@ public class Elegance extends AppCompatActivity  {
         });
 
 
-        eventName_list.add("Bridge");
-        eventName_list.add("Paper Bridge");
-        eventName_list.add("Building");
-        eventName_list.add("Paper Bridge");
-        eventName_list.add("Paper Rasta");
-        eventName_list.add("Pach Pach Rasta");
-        eventName_list.add("Pattal zali mala");
+        eventName_list.add("Rangmanch");
+        eventName_list.add("Kaviya");
+        eventName_list.add("Dhindhora");
+        eventName_list.add("Lasya");
+        eventName_list.add("Synchro");
+        eventName_list.add("Aalap");
+        eventName_list.add("Crescendo");
+        eventName_list.add("T-Shirt Painting");
+        eventName_list.add("Get. Set.. Paint...");
+        eventName_list.add("Masquerade");
+
+        eventDetails_list.add("Cause theatres were created to tell the truth about life! \n" +
+                "\n RULES:\n\n" +
+                "\uF071 Only solo performances are allowed.\n" +
+                "\uF071 The art can be performed in any language of comfort.\n" +
+                "\uF071 No offensive words are permissible. The performer will be disqualified if abusive content is found. \uF071 Performance will be judged purely on acting. Background music is not needed.\n" +
+                "\uF071 The participants are allowed to bring their own properties . Only chair & table will be provided. \uF071 Participants have to report before 1 hour the competition to avoid disqualification.\n" +
+                "\uF071 \n Registration Fees : INR 150\n\n" +
+                "\uF071 \nPrizes : ₹ 2500 & ₹2000\n" +
+                "\uF071 \n Time Duration : 6 mins (+1 minute stage set up & clearance \n)");
+        eventDetails_list.add("Witness the world trapped in a poet being exploited with a hint of rhyme! \n" +
+                "\n RULES:\n\n" +
+                "\uF071This event consists of one round\n" +
+                "\uF071 Participants should present a 2 self-prepared poems.\n" +
+                "\uF071Language used can be Hindi or Marathi only.\n" +
+                "\uF071The judging depends upon the content of the poem along with presentation.\n" +
+                "\uF071The performance has to be presented on a stage with audience on one side.\n" +
+                "\uF071 Participants have to report 1 hour before the competition or else that team faces disqualification. \uF071Registration fees: INR 150\n" +
+                "\uF071 \nPrizes: ₹ 1500 & ₹1000\n" +
+                "\uF071 \n Time Duration: 5 mins\n");
+        eventDetails_list.add("Talent has always been recognized by the ones who never fail to showcase it. A stage is where an act is performed but a real act makes every place a stage itself! \n" +
+                "\n RULES:\n\n" +
+                "\uF071 This event consists of one round\n" +
+                "\uF071 The essence of the play should be brought out in any language.\n" +
+                "\uF071 The team size represents the number of people registered as a team. Only these shall be allowed to perform the street play. \uF071 Music accompanists are included in the team size stated above.\n" +
+                "\uF071 The act has to be performed on a circular (8 meter in diameter) stage with audience on all the sides.\n" +
+                "\uF071 The stage must be cleared by the participants to avoid problems for the next team or else heavy penalties will be imposed. \uF071 Teams have to report 1 hour before the competition or else that team faces disqualification.\n" +
+                "\uF071 \nRegistration Fees : INR 700\n" +
+                "\uF071 \nNo. of participants per team: 8-20.\n" +
+                "\uF071 \nPrizes : ₹ 5000, ₹ 4000\n" +
+                "\uF071 \n Time Duration: 15 minutes (including the entry and clearance time)\n");
+        eventDetails_list.add("“One for all. All for one!” \n" +
+                "\n RULES:\n\n" +
+                "\uF071Only SOLO Performances are allowed.\n" +
+                "\uF071Any dance style and song selection is allowed.\n" +
+                "\uF071Vulgar songs are strictly prohibited.\n" +
+                "\uF071Over-exposing costumes are not allowed.\n" +
+                "\uF071Video auditions to be submitted from 28th Sept to 2nd Oct to the respective co-ordinators. \uF071Time limit for video auditions – 1 minute.\n" +
+                "\uF071Only dances which clears auditions are allowed to perform.\n" +
+                "\uF071Props are allowed.\n" +
+                "\uF071Participant teams have to report before 1 hour of the competition to avoid disqualification. \uF071 Time Limit for performance -3 - 5 min (+1 preparation & stage clearance)\n" +
+                "\uF071Registration Fee : INR 200\n" +
+                "\uF071\nPrizes : ₹2500, ₹2000\n");
+        eventDetails_list.add("Dance brings out the best in you & so does the team work. Then why not bring them together itself? Reveal your coordination in the most beautiful art form of your choice. Join us in SYNCHRO!\n" +
+                "\n RULES:\n\n" +
+                "\uF071 This event comprises of only one round of GROUP Dance.\n" +
+                "\uF071 Any dance style and song selection is allowed.\n" +
+                "\uF071Vulgar songs are strictly prohibited.\n" +
+                "\uF071Over-exposing costumes are not allowed.\n" +
+                "\uF071 Video auditions to be submitted from 28th Sept to 2nd Oct to the respective co-ordinators. \uF071 Time limit for video auditions – 1 minute.\n" +
+                "\uF071Only dances which clears auditions are allowed to perform\n" +
+                "\uF071Participant teams have to report before 1 hour of the competition to avoid disqualification. \uF071 Props are allowed.\n" +
+                "\uF071The limit of performance – 3-5 minutes (+1 preparation & stage clearance)\n" +
+                "\uF071Registration Fee : INR 700\n" +
+                "\uF071No. of Participants : 7-12.\n" +
+                "\uF071\nPrizes : ₹ 5000, ₹ 4000\n");
+        eventDetails_list.add("“Sing your feelings.. Not just words!”\n" +
+                "\n RULES:\n\n" +
+                " \uF071This event comprises of 1 round.\n" +
+                "\uF071Video auditions to be submitted from 28th Sept to 2nd Oct to the respective co-ordinators.\n" +
+                "\uF071Time limit for video auditions – 1 minute.\n" +
+                "\uF071Background music not compulsory.\n" +
+                "\uF071You may also bring a karaoke track.\n" +
+                "\uF071Judging is purely based on the singing.\n" +
+                "\uF071 No RAPS allowed. Vulgarity/Obscenity is prohibited.\n" +
+                "\uF071 Participants have to report 1 hour before the competition or else that team faces disqualification.\n" +
+                "\uF071One instrumentalist may accompany the participant. The accompanist can either be a guitarist or a synth player who should\n" +
+                "get his/her instrument.\n" +
+                "\uF071 Registration fees: INR 150 \uF071 \n" +
+                "\uF071\nPrizes: ₹ 2000, ₹ 1500, ₹ 1000\n");
+        eventDetails_list.add("“Singing is an act of presenting your emotions wrapped in melody.”\n" +
+                "\n RULES:\n\n" +
+                "\uF071 The event consist of only one round.\n" +
+                "\uF071 At least one song has to be performed and at least 3 instruments have to be used.\n" +
+                "\uF071 The songs performed can be a cover or OC in either English, Hindi or Marathi or all three.\n" +
+                "\uF071 All the instruments except the Drum kit should be brought by the Band itself.\n" +
+                "\uF071 The Band will be provided with a basic drum kit, drum throne, 5 Amps (3 for base guitars, 2 for electric/acoustic guitars),\n" +
+                "synth stand, mics and line-in jacks for guitars.\n" +
+                "\uF071 Participants are welcome to bring their own equipments(Amps, drum pieces etc.)\n" +
+                "\uF071 Vocals with lyrics are mandatory.\n" +
+                "\uF071 A time slot of 2 hours before the event starts is allotted for the Bands to have a sound check and configure the levels.\n" +
+                "\uF071 Bands who do not wish to have a sound check should report 1 hour before the competition or else that band faces\n" +
+                "disqualification.\n" +
+                "\uF071 Registration fees: INR 1000\n" +
+                "No. of participants per team: 4-8\n" +
+                "\n Time Duration: 10 minutes (+3 mins setup and music check time) \n" +
+                "\uF071\nPrizes: ₹ 8000, ₹ 5000\n\n");
+        eventDetails_list.add("Wear your imagination!\n" +
+                "\n RULES:\n\n" +
+                " \uF071This event consists of 1 round.\n" +
+                "\uF071Participants will be provided with T shirts.\n" +
+                "\uF071Colors, sponge and other decorative material will be provided. Brushes will not be provided. \uF071Participants can also use their own decorative material if allowed by volunteers.\n" +
+                "\uF071Any copied or printed material is strictly prohibited. However, any source inspired idea is welcome. \uF071 Participants have to report 1 hr before the competition to avoid disqualification.\n" +
+                "\uF071Registration Fee : INR 120/-\n" +
+                "\uF071\n Time Duration : 60 minutes \n" +
+                "\uF071\nPrizes : We are not gonna reveal this . 3 best painting with some proper cash prizes & gift hampers \n");
+
+        eventDetails_list.add("This event consists of one round.\n" +
+                "\n RULES:\n\n" +
+                "All the necessary material will be provided (A2 Sheet, colours, sponge) except brushes Colours will be provided by volunteers as required not all at once.\n" +
+                "Participants are not allowed to use personal materials or copy from any kind of objectionable source (mobiles, laptops, photographs etc.) during the course of the competition.\n" +
+                "Participants will be judged on the basis of creativity, innovation, design & LEAST time taken to complete the art. Participants have to report 1 hour before the competition to avoid disqualification.\n" +
+                "Registration Fees :INR 100/-\n \nPrizes : ₹1500 & ₹ 1000\n");
+        eventDetails_list.add("\n" +
+                "\uF071 This event comprises of ONE Round of face painting.\n" +
+                "\uF071 It is a team event . There can be only 2 participants in a team.\n" +
+                "\uF071 Basic colours, sponge will be provided. Participants should bring their own brushes. \uF071 Topic will be given on the spot.\n" +
+                "\uF071 Participants will be judged on the basis of creativity, innovation & design.\n" +
+                "\uF071 Decision of the judges will be final & binding.\n" +
+                "\uF071 Registration fees : INR 100/- per TEAM.\n" +
+                "\uF071 Time limit : 60 minutes \n \nPrizes : We are not gonna reveal this . 3 best painting with some proper cash prizes & gift hampers\n");
+
+        eventLocation.add("Electrical seminar hall");
+        eventLocation.add("Electrical seminar hall");
+        eventLocation.add("In front of Applied Mechanics");
+        eventLocation.add("College Auditorium");
+        eventLocation.add("College Auditorium");
+        eventLocation.add("In front of Classroom Complex");
+        eventLocation.add("Open Stage behind Civil Dept");
+        eventLocation.add("Applied Mechanics");
+        eventLocation.add("Applied Mechanics");
+        eventLocation.add("Applied Mechanics");
+
+        eventContactPerson_list.add("Priyanka Rajput");
+        eventContactPerson_list.add("Dipam Chaudhari");
+        eventContactPerson_list.add("Tejas Bagul");
+        eventContactPerson_list.add("Payal Sarkate");
+        eventContactPerson_list.add("Nikita Wadgaonkari");
+        eventContactPerson_list.add("Chinmay Kulkarni");
+        eventContactPerson_list.add("Ganesh Kasle");
+        eventContactPerson_list.add("Pramita Shahare");
+        eventContactPerson_list.add("Amruta Kulkarni");
+        eventContactPerson_list.add("Amruta Kulkarni");
+
+        eventContactNum_list.add("8805895988");
+        eventContactNum_list.add("7798040995");
+        eventContactNum_list.add("8275682235");
+        eventContactNum_list.add("9850644422");
+        eventContactNum_list.add("7040920114");
+        eventContactNum_list.add("9881246601");
+        eventContactNum_list.add("9423781980");
+        eventContactNum_list.add("9975531168");
+        eventContactNum_list.add("8087966802");
+        eventContactNum_list.add("8087966802");
+
+        eventDate.add("7 OCT 2017");
+        eventDate.add("8 OCT 2017");
+        eventDate.add("8 OCT 2017");
+        eventDate.add("8 OCT 2017");
+        eventDate.add("8 OCT 2017");
+        eventDate.add("7 OCT 2017");
+        eventDate.add("7 OCT 2017");
+        eventDate.add("7 OCT 2017");
+        eventDate.add("8 OCT 2017");
+        eventDate.add("7 OCT 2017");
 
 
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt lskddjasli fjsdfljdfiods jsjhfkdsjhflkjsahesNV OIV OeifjfNSDJ NODISHENFNJSAF sdvds vldskvn slknsdlndsiv disVldnvlds knlsVD LSDK");
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt.amet, consectetur adipisicing elit. Sunt, amet, consectetur adipisicing elit. Sunt");
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur, amet, consectetur adipisicing elit. Suntm amet, consectetur adipisicing elit. Sunt adipisicing elit. Sunt");
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt");
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt");
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt");
-        eventDetails_list.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt");
-
-        eventLocation.add("Elegance");
-        eventLocation.add("CSE Department");
-        eventLocation.add("Workshop");
-        eventLocation.add("The Grounds");
-        eventLocation.add("Classroom Complex");
-        eventLocation.add("Qwerty Complex");
-        eventLocation.add("Qwerty Complex");
-
-        eventContactPerson_list.add("Ashwin Kulkarni");
-        eventContactPerson_list.add("Saurabh Kulkarni");
-        eventContactPerson_list.add("Daru Kulkarni");
-        eventContactPerson_list.add("Ashwin Kulkarni");
-        eventContactPerson_list.add("Saurabh Kulkarni");
-        eventContactPerson_list.add("Bochya Kulkarni");
-        eventContactPerson_list.add("Ashwin Kulkarni");
-
-        eventContactNum_list.add("7798080437");
-        eventContactNum_list.add("7798080437");
-        eventContactNum_list.add("7798080437");
-        eventContactNum_list.add("7798080437");
-        eventContactNum_list.add("7798080437");
-        eventContactNum_list.add("1798080437");
-        eventContactNum_list.add("1798080437");
-
-        eventDate.add("1 OCT 2017");
-        eventDate.add("2 OCT 2017");
-        eventDate.add("3 OCT 2017");
-        eventDate.add("4 OCT 2017");
-        eventDate.add("5 OCT 2017");
-        eventDate.add("9 OCT 2017");
-        eventDate.add("19 OCT 2017");
-
-        eventprice.add("35");
-        eventprice.add("45");
-        eventprice.add("50");
-        eventprice.add("25");
-        eventprice.add("50");
-        eventprice.add("90");
-        eventprice.add("90");
+        eventprice.add("150");
+        eventprice.add("150");
+        eventprice.add("500");
+        eventprice.add("200");
+        eventprice.add("600");
+        eventprice.add("150");
+        eventprice.add("800");
+        eventprice.add("150");
+        eventprice.add("100");
+        eventprice.add("100");
 
 
         EleganceAdapter ad = new EleganceAdapter(Elegance.this, eventName_list, eventDetails_list, eventLocation, eventContactPerson_list, eventContactNum_list, eventDate, eventprice);
